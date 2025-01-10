9147b294ce5db4eea7581441f321da202d4a3c10
Tokiodan <danibouzidi7@gmail.com>
2025-01-10 14:12:24 +0100

    BugFolder
    Created a folder where we can put text files in for bugs

--- Assets/Scripts/StructureManager.cs
+++ Assets/Scripts/StructureManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 public class StructureManager : MonoBehaviour {
     [SerializeField] private GridManager _gridManager;
 
- 
+   
     [SerializeField] private Button _houseButton;
     [SerializeField] private Button _farmButton;
     [SerializeField] private Button _windmillButton;
@@ -19,14 +19,14 @@ public class StructureManager : MonoBehaviour {
     private GameObject _currentGhostStructure;
 
     void Start() {
-        
+      
         _houseButton.onClick.AddListener(() => SetCurrentStructurePrefab(_housePrefab));
         _farmButton.onClick.AddListener(() => SetCurrentStructurePrefab(_farmPrefab));
         _windmillButton.onClick.AddListener(() => SetCurrentStructurePrefab(_windmillPrefab));
         _workshopButton.onClick.AddListener(() => SetCurrentStructurePrefab(_workshopPrefab));
     }
 
-    
+   
     public void SetCurrentStructurePrefab(GameObject structurePrefab) {
         if (_currentGhostStructure != null) {
             Destroy(_currentGhostStructure); 
@@ -34,16 +34,23 @@ public class StructureManager : MonoBehaviour {
 
         _currentGhostStructure = Instantiate(structurePrefab, Vector3.zero, Quaternion.identity); 
         _currentGhostStructure.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f, 0.5f); 
+    }
 
     void Update() {
-       
+  
         if (_currentGhostStructure != null) {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
             mousePos.z = 0f; 
-            _currentGhostStructure.transform.position = new Vector3(Mathf.Floor(mousePos.x), Mathf.Floor(mousePos.y), 0f);
+
+         
+            Vector2 gridPos = new Vector2(Mathf.Floor(mousePos.x), Mathf.Floor(mousePos.y));
+            _currentGhostStructure.transform.position = new Vector3(gridPos.x, gridPos.y, 0f);
+
+            
+            Debug.Log($"Ghost structure position: {gridPos}");
         }
 
-        
+    
         if (_currentGhostStructure != null && Input.GetMouseButtonDown(0)) {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 gridPos = new Vector2(Mathf.Floor(mousePos.x), Mathf.Floor(mousePos.y));
@@ -55,5 +62,4 @@ public class StructureManager : MonoBehaviour {
             }
         }
     }
-}
 }
